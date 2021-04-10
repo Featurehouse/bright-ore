aef46a007b721ba2f853888b4eeeda041d62e5e5
teddyxlandlee <tedexe_work.top@foxmail.com>
2021-04-10 19:20:12 +0800

    initial commit

---
+++ src/main/java/org/featurehouse/mcmod/brightore/BrightOreClient.java
@@ -0,0 +1,13 @@
+package org.featurehouse.mcmod.brightore;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class BrightOreClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
