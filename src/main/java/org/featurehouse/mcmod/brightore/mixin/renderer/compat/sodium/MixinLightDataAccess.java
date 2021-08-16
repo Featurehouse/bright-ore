52e92df9e2f6858100eea0eef1a50f1a674456f0
Teddy Li <teddyxlandlee@hotmail.com>
2021-08-16 14:11:06 +0800

    Redirect ref

--- src/main/java/org/featurehouse/mcmod/brightore/mixin/renderer/compat/sodium/MixinLightDataAccess.java
+++ src/main/java/org/featurehouse/mcmod/brightore/mixin/renderer/compat/sodium/MixinLightDataAccess.java
@@ -1,12 +1,15 @@
 package org.featurehouse.mcmod.brightore.mixin.renderer.compat.sodium;
 
 import me.jellysquid.mods.sodium.client.model.light.data.LightDataAccess;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.block.BlockState;
 import org.featurehouse.mcmod.brightore.OreTagLoader;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+@Environment(EnvType.CLIENT)
 @Mixin(LightDataAccess.class)
 public class MixinLightDataAccess {
     @Redirect(method = "compute(III)J", at = @At(
--- src/main/java/org/featurehouse/mcmod/brightore/mixin/renderer/compat/sodium/MixinSodiumBlockRenderer.java
+++ src/main/java/org/featurehouse/mcmod/brightore/mixin/renderer/compat/sodium/MixinSodiumBlockRenderer.java
@@ -1,17 +1,18 @@
 package org.featurehouse.mcmod.brightore.mixin.renderer.compat.sodium;
 
 import me.jellysquid.mods.sodium.client.render.pipeline.BlockRenderer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.block.BlockState;
 import org.featurehouse.mcmod.brightore.OreTagLoader;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+@Environment(EnvType.CLIENT)
 @Mixin(BlockRenderer.class)
 public class MixinSodiumBlockRenderer {
-    @Redirect(method = "getLightingMode(Lnet/minecraft/block/BlockState;" +
-            "Lnet/minecraft/client/render/model/BakedModel;)" +
-            "Lme/jellysquid/mods/sodium/client/model/light/LightMode;",
+    @Redirect(method = "getLightingMode",
     at = @At(value = "INVOKE", target = "net.minecraft.block.AbstractBlock.AbstractBlockState.getLuminance()I"))
     private int redirectLuminance(BlockState it) {
         return OreTagLoader.redirectLuminance(it);
